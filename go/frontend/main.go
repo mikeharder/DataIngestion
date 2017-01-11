dde4773858623b676c560f06bfb7eb4a753f3a2b
Mike Harder <mharder@microsoft.com>
2017-01-11 23:57:47 +0000

    Frontend accepts JSON payload Script to test service with curl

--- go/frontend/main.go
+++ go/frontend/main.go
@@ -1,12 +1,41 @@
 package main
 
 import (
-    "fmt"
+    "bytes"
+    "encoding/json"
     "net/http"
 )
 
+type Payload struct {
+    Data string `json:"data"`
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!\n", r.URL.Path[1:])
+    decoder := json.NewDecoder(r.Body)
+
+    var p Payload
+    err := decoder.Decode(&p)
+    if err != nil {
+        http.Error(w, http.StatusText(400), 400)
+    }
+
+/*
+    err = redirectPayload(p, "http://go-backend:8080")
+    if err != nil {
+        http.Error(w, http.StatusText(502), 502)
+    }
+*/
+}
+
+func redirectPayload(p Payload, url string) error {
+    b := new(bytes.Buffer)
+    json.NewEncoder(b).Encode(p)
+    r, err := http.Post(url, "application/json; charset=utf-8", b)
+    if err != nil {
+        return err
+    }
+    defer r.Body.Close()
+    return nil
 }
 
 func main() {
---
+++ go/test.sh
@@ -0,0 +1,1 @@
+curl -v -H "Content-Type: application/json" -d @payload.json ${1:-http://localhost:8080/}
