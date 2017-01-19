ceb4d165b0f0772f147c525db17cd5a87f4f121b
Mike Harder <mharder@microsoft.com>
2017-01-18 18:09:39 -0800

    Add option to use HttpClientSlim

--- aspnetcore/frontend/Startup.cs
+++ aspnetcore/frontend/Startup.cs
@@ -1,15 +1,17 @@
-using System;
-using System.IO;
-using System.Net.Http;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Newtonsoft.Json;
+using Microsoft.AspNetCore.Testing;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net.Http;
 using System.Threading;
+using System.Threading.Tasks;
 
-namespace Frontend {
+namespace Frontend
+{
     public class Startup
     {
         private static readonly PathString _path = new PathString("/ingest/event");
@@ -42,7 +44,9 @@ namespace Frontend {
             bool tempBool;
             var expectContinue = bool.TryParse(Configuration["expectContinue"], out tempBool) ? (bool?)tempBool : null;
 
-            Console.WriteLine($"Clients: {clients}, ExpectContinue: {expectContinue?.ToString() ?? "null"}");
+            var clientType = String.IsNullOrEmpty(Configuration["clientType"]) ? "full" : Configuration["clientType"];
+
+            Console.WriteLine($"ClientType: {clientType}, Clients: {clients}, ExpectContinue: {expectContinue?.ToString() ?? "null"}");
 
             app.Run(async context =>
             {
@@ -53,7 +57,7 @@ namespace Frontend {
                         payload = _jsonSerializer.Deserialize<Payload>(reader);
                     }
 
-                    using (var response = await RedirectPayload(payload, "http://aspnetcore-backend:8080/ingest/data", expectContinue))
+                    using (var response = await RedirectPayload(payload, "http://aspnetcore-backend:8080/ingest/data", expectContinue, clientType))
                     {
                     }
                 }
@@ -69,18 +73,31 @@ namespace Frontend {
             return _httpClients[Interlocked.Increment(ref _httpClientCounter) % _httpClients.Length];
         }
 
-        private static async Task<HttpResponseMessage> RedirectPayload(Payload payload, string url, bool? expectContinue) {
+        private static async Task<HttpResponseMessage> RedirectPayload(Payload payload, string url, bool? expectContinue, string clientType) {
             using (var stream = new MemoryStream())
             using (var writer = new StreamWriter(stream))
+            using (var content = new StreamContent(stream))
             {
                 _jsonSerializer.Serialize(writer, payload);
                 writer.Flush();
                 stream.Seek(0, SeekOrigin.Begin);
 
-                var m = new HttpRequestMessage(HttpMethod.Post, url);
-                m.Content = new StreamContent(stream);
-                m.Headers.ExpectContinue = expectContinue;
-                return await NextHttpClient().SendAsync(m);
+                if (clientType == "full")
+                {
+                    var m = new HttpRequestMessage(HttpMethod.Post, url);
+                    m.Content = content;
+                    m.Headers.ExpectContinue = expectContinue;
+                    return await NextHttpClient().SendAsync(m);
+                }
+                else if (clientType == "slim")
+                {
+                    var responseString = await HttpClientSlim.PostAsync(url, content);
+                    return new HttpResponseMessage() { Content = new StringContent(responseString) };
+                }
+                else
+                {
+                    throw new InvalidOperationException();
+                }
             }
         }
 
